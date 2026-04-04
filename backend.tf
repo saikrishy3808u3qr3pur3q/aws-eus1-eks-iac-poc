45e1b106a59067e8bdce14459d98ac186bc9660b
Saikrishnan R <saikrish050903@gmail.com>
2026-04-04 20:05:36 +0530

    Fix - (Update working directory)

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "your-terraform-state-bucket"
-#     key     = "eks/networking/terraform.tfstate"
-#     region  = "us-east-1"
-#     encrypt = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket  = "aws-eus1-eks-statefile"
+    key     = "eks/networking/terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
